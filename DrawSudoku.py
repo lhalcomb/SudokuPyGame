eca21b24826c7e12c7dc556e143ccb96a1c40a51
lhalcomb <lhalcomb@drury.edu>
2025-01-10 01:17:01 -0600

    optimized screen sizing for sudoku board

--- DrawSudoku.py
+++ DrawSudoku.py
@@ -4,13 +4,14 @@ black = (0, 0, 0)
 class drawSudoku:
     def __init__(self, cellSize):
         self.cellSize = cellSize
+        
 
 
-    def drawGrid(self, screen):
+    def drawGrid(self, screen, size):
         for i in range(10):
             line_thickness = 3 if i % 3 == 0 else 1
-            pygame.draw.line(screen, black, (0, i * self.cellSize), (540, i * self.cellSize), (line_thickness))
-            pygame.draw.line(screen, black, (i * self.cellSize, 0), ( i * self.cellSize, 540), (line_thickness))
+            pygame.draw.line(screen, black, (0, i * self.cellSize), (size, i * self.cellSize), (line_thickness))
+            pygame.draw.line(screen, black, (i * self.cellSize, 0), ( i * self.cellSize, size), (line_thickness))
 
     def draw_numbers(self, screen, font, grid):
         for row in range(9):
