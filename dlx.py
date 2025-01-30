83f6eb66163b08481c5f8367104d4f6e502b7927
lhalcomb <lhalcomb@drury.edu>
2025-01-30 15:42:39 -0600

    created incidence matrix file and then dlx.py class for algorithm x

---
+++ dlx.py
@@ -0,0 +1,5 @@
+from incidence_matrix import Cell, Column, Matrix2D, IncidenceMatrix
+
+
+class dlx: 
+    pass
--- incidence_matrix.py
+++ incidence_matrix.py
@@ -1,24 +1,139 @@
+from typing import List
 
+Matrix2D = List[List[int]]
 
 class Cell:
-    def __init__(sellf, self, left = None, right = None, up = None, down = None, col = None):
+    def __init__(self, name = None, left = None, right = None, up = None, down = None, col = None):
         self.left = left
         self.right = right
         self.up = up
         self.down = down
         self.col = col
+        self.name = name
 
-    def __repr__(self) -> str:
-        return f'Cell({self.name}, left = {self.left}, right = {self.right}, up = {self.up}, down = {self.down}, colHeader = {self.col})'
+    # def __repr__(self) -> str:
+    #     return f'Cell({self.name}, left = {self.left}, right = {self.right}, up = {self.up}, down = {self.down}, colHeader = {self.col})'
         
-    def __str__(self) -> str:
-        return self.name
+    # def __str__(self) -> str:
+    #     return self.__repr__()
 
-class ColumnHeader(Cell):
-    def __init__(self, name, size = 0, *args, **kwargs) -> None:
-        super().__init__(name, *args, **kwargs)
+class Column(Cell):
+    def __init__(self, size = 0, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.size = size        
 
-class Root(ColumnHeader):
+class Root(Column):
     def __init__(self) -> None:
         super().__init__(name = "root")
+
+class IncidenceMatrix: 
+    def __init__(self, grid: Matrix2D):
+        self.header: Root = Root()
+        self.grid = grid
+        self.create_dbly_linked_list()
+    
+    def grid(self) -> Matrix2D:
+        return self.grid
+    
+    def create_dbly_linked_list(self):
+        self.create_columns()
+        self.connect_rows()
+
+    def create_columns(self) -> None:
+        prev_col = self.header
+        for col_index in range(len(self.grid[0])):
+            name = f"col->{col_index}"
+            col = Column(size=0, name=name)
+            col.left = prev_col
+            prev_col.right = col
+            prev_row = col
+            for cell_index in range(len(self.grid)):
+                if self.grid[cell_index][col_index] != 1:
+                    continue
+                row = Cell(name=f"row->{cell_index}", col=col)
+                col.size += 1
+                row.up = prev_row
+                prev_row.down = row
+                prev_row = row
+            col.up = prev_row
+            prev_row.down = col
+            prev_col = col
+        self.header.left = prev_col
+        prev_col.right = self.header
+
+    def connect_rows(self) -> None:
+        for row in range(len(self.grid)):
+            ones_in_row = self.ones_in_row(row)
+
+            if len(ones_in_row) == 0:
+                continue
+
+            first_cell = self.get_cell_from_columns(row, ones_in_row[0])
+            prev_cell = first_cell
+
+            for col in ones_in_row[1:]:
+                cell = self.get_cell_from_columns(row, col)
+
+                cell.left = prev_cell
+                prev_cell.right = cell
+
+                prev_cell = cell
+            
+            first_cell.left = prev_cell
+            prev_cell.right = first_cell
+
+    def get_cell_from_columns(self, row_index, col_index) -> Cell | None:
+        if self.grid[row_index][col_index] == 0:
+            return None
+        
+        col = self.header
+
+        while col.name != f"col->{col_index}":
+            col = col.right
+
+        cell = col
+
+        while cell.name != f"row->{row_index}":
+            cell = cell.down
+
+        return cell
+
+    def ones_in_row(self, row) -> int:
+            ones = []
+            
+            for row in range(len(self.grid[0])):
+                if self.grid[row] == 1:
+                    ones.append(row)
+            
+            return ones
+    def print_sparse_matrix(self): 
+        cells: List[List[str]] = [[self.header.name]]
+        col = self.header.right
+
+        while col is not self.header:
+            row_cells = [col.name]
+
+            row = col.down
+
+            while row is not col:
+                row_cells.append(row.name)
+
+                row = row.down
+            row_cells.append(f"column size->{str(col.size)}")  #checking col size
+            col = col.right
+            
+            cells.append(row_cells)
+
+        for row in cells:
+            print(', '.join(row))
+
+
+if __name__ == "__main__":
+    grid_noSol = [
+            [1, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 0],
+            [1, 0, 1, 1]]
+    
+    sparse_matrix = IncidenceMatrix(grid_noSol)
+    sparse_matrix.print_sparse_matrix()
